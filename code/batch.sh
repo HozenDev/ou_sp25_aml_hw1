4e2b31696c8f76c726b2cfafa566af2fff10e28b
Hozen <enzo.durel@gmail.com>
2025-02-12 16:20:19 -0600

    test

--- code/batch.sh
+++ code/batch.sh
@@ -49,7 +49,7 @@ python hw1_base_skel.py --exp_type $EXPERIMENT_TYPE \
                          --activation_out 'linear' \
                          --activation_hidden 'relu' \
                          --epochs 500 \
-                         --hidden 20 10 \
-                         --lrate 0.0001 \
+                         --hidden 10 5 \
+                         --lrate 0.001 \
                          -vv
 
